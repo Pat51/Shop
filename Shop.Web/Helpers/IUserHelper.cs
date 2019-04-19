63ef8518c42da38a47c3fb843d0ff959a9cd4fe2
daniel mandarano <danielmandarano@yahoo.com.ar>
2019-04-19 20:36:27 -0300

    Repo Generico y countries

---
+++ Shop.Web/Helpers/IUserHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Shop.Web.Helpers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Data.Entities;
+    using Microsoft.AspNetCore.Identity;
+    using Models;
+
+
+
+    public interface IUserHelper
+    {
+        Task<User> GetUserByEmailAsync(string email);
+
+        Task<IdentityResult> AddUserAsync(User user, string password);
+
+    }
+}
